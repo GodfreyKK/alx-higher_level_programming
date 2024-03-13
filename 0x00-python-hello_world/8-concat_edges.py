1c74a092dc13e4a34f8b19980c7f21fea04a87a5
GodfreyKK <godfreykevink@gmail.com>
2024-03-13 12:43:01 -0700

    creating won string

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\language that combines remarkable power with very clear syntax"
+x = 'object-oriented programming with Python'
+str = 'object-oriented programming with Python' + str[len(x):0]
+print(str)
