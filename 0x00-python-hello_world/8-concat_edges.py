81101b21991449cab91e0327347f9d3a60b0ac1e
GodfreyKK <godfreykevink@gmail.com>
2024-03-13 12:53:05 -0700

    ommited slicing and assigned directly to str

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\language that combines remarkable power with very clear syntax"
-x = 'object-oriented programming with Python'
-str = 'object-oriented programming with Python' + str[len(x) - 1]
+str = "object-oriented programming with Python"
 print(str)
